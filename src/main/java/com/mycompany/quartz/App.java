6c1f1deee092adce4277d3d772f27c47c7aed6ea
Colin But <colinbut@users.noreply.github.com>
2017-10-23 16:23:46 +0100

    Instantiate a scheduler and start it

--- src/main/java/com/mycompany/quartz/App.java
+++ src/main/java/com/mycompany/quartz/App.java
@@ -1,13 +1,22 @@
 package com.mycompany.quartz;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
+
+public class App {
+
+    public static void main( String[] args ) {
+        try {
+            // Schedulers are created from Factories
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+
+            // Start the scheduler
+            scheduler.start();
+
+
+        } catch (SchedulerException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
     }
 }

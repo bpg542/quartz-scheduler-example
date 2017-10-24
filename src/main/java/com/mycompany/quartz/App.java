7b8a8e1bbd2e0607a1e0688d7526e18606f07a38
Colin But <colinbut@users.noreply.github.com>
2017-10-24 21:57:59 +0100

    Scheduling the job to run

--- src/main/java/com/mycompany/quartz/App.java
+++ src/main/java/com/mycompany/quartz/App.java
@@ -1,8 +1,14 @@
 package com.mycompany.quartz;
 
+import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
+import org.quartz.Trigger;
 import org.quartz.impl.StdSchedulerFactory;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 public class App {
 
@@ -14,6 +20,24 @@ public class App {
             // Start the scheduler
             scheduler.start();
 
+            /*
+             * Scheduling those jobs with Triggers that define what time the jobs should run
+             */
+
+            // Defining the job (and tying it to MyJob.class)
+            JobDetail jobDetail = newJob(MyJob.class)
+                .withIdentity("job1", "group1")
+                .build();
+
+            // Trigger the job to run at what time and frequency
+            Trigger trigger = newTrigger()
+                .withIdentity("trigger1", "group 1")
+                .startNow()
+                .withSchedule(simpleSchedule().withIntervalInMilliseconds(1000L).repeatForever())
+                .build();
+
+            // tell scheduler to schedule the job using the trigger
+            scheduler.scheduleJob(jobDetail, trigger);
 
         } catch (SchedulerException e) {
             System.out.println(e.getLocalizedMessage());
